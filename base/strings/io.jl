1aa16e29238ba13bc497469267b12951ed3ad2ea
Tim Holy <tim.holy@gmail.com>
2020-08-24 07:36:06 -0500

    Add nospecialize to _str_sizehint
    We have one method which is written in a way that suggests it's
    intended to be a singleton. However, there are 56 MethodInstances for
    different concrete types.

--- base/strings/io.jl
+++ base/strings/io.jl
@@ -107,7 +107,7 @@ function sprint(f::Function, args...; context=nothing, sizehint::Integer=0)
     String(resize!(s.data, s.size))
 end
 
-function _str_sizehint(x)
+function _str_sizehint(@nospecialize x)
     if x isa Float64
         return 20
     elseif x isa Float32
